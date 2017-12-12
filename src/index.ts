eaedcba9644b6d37a629fb0e4a6a599160191e52
Giovanni Gonzaga <giovanni@buildo.io>
2017-12-12 16:58:59 +0100

    re-export also CookiesProvider from react-cookie

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import CookieBannerUniversal from './CookieBannerUniversal';
 import BannerContent from './BannerContent';
 
 export default CookieBanner;
-export { Cookies } from 'react-cookie';
+export { Cookies, CookiesProvider } from 'react-cookie';
 export { BannerContent, CookieBannerUniversal }
